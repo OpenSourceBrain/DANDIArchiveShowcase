f2952479bd1401797cc825d1dd053616f61fb27b
Daksh <daksh.rathore9@gmail.com>
2023-07-08 18:58:33 +0530

    Made changes to shell file

--- generateDocker.sh
+++ generateDocker.sh
@@ -2,6 +2,7 @@ docker build --platform linux/amd64 --network host -t dandi_docker .
 docker run --privileged -v "$(pwd)":/testing --network=host -dit --name=da dandi_docker 
 docker start da
 echo done
+docker exec da /bin/sh -c 'ln -s ~/.docker/run/docker.sock /var/run/docker.sock'
 docker exec da /bin/sh -c 'docker build -t nwbe OSBv2/applications/nwb-explorer/.'
 echo on last
 docker exec da /bin/sh -c 'docker run -dit --name=nwbe -v "$(pwd)"testing:/home/jovyan/nwb-explorer/testing nwbe'
