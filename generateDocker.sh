fce5ed782a2ae14b6ef600dca0125a01ecb5f58c
Daksh <daksh.rathore9@gmail.com>
2023-07-09 16:24:39 +0530

    Added different entry points for dind container

--- generateDocker.sh
+++ generateDocker.sh
@@ -1,8 +1,11 @@
-docker build -t dandi_docker .
-docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock -d --name=da dandi_docker 
+docker build --network=host -t dandi_docker .
+docker run -v /var/run/docker.sock:/var/run/docker.sock -d -t --name=da dandi_docker tail -f /dev/null
+
+docker exec da docker --version
 echo step1
 docker exec da  docker container ps
 echo step2
 docker exec da  docker build -t nwbe OSBv2/applications/nwb-explorer/.
 echo step3
+docker run da
 docker exec da docker run -dit --name=nwbe -v "$(pwd)"testing:/home/jovyan/nwb-explorer/testing nwbe
