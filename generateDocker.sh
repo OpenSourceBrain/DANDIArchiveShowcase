60247a122597b2531907a1ad5f36577e381f6f92
Padraig Gleeson <p.gleeson@gmail.com>
2023-08-07 13:35:30 +0100

    Cleaning up previous docker containers etc at start of generation

--- generateDocker.sh
+++ generateDocker.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+set -ex
+
+# Clean up if previously run
+docker stop nwbe || echo Docker container nwbe not running
+docker rm nwbe || echo Docker container nwbe not running
+docker stop da || echo Docker container da not running
+docker rm da || echo Docker container da not running
+
 docker build --network=host -t dandi_docker .
 docker volume create --name tmpf
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/testing -v tmpf:/tmp -d --name=da dandi_docker tail -f /dev/null
